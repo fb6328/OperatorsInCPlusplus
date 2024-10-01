8a69d5ccbc79408873688d12138f035297cf385f
FredB <fb6328@gmail.com>
2024-10-01 18:39:23 +0300

    Update

---
+++ ArrayList.cpp
@@ -0,0 +1,16 @@
+// Implementing a Linked list using array in c++
+#include <iostream>
+using namespace std;
+
+// Define the maximum size of the array
+#define MAX_SIZE 100
+
+// Define the structure of the linked list
+struct Node
+{
+    int data;
+    int next;
+};
+/*end{code} The code snippet provided is incomplete and does not include the implementation of the linked list operations.However,
+    it defines the structure of the linked list using an array in C++.The structure `Node` contains two fields : `data` to store the value of the node and `next` to store the index of the next node in the array.The `#define ` directive is used to define a constant `MAX_SIZE` which represents the maximum
+        size of the array used to store the linked list nodes.*/
